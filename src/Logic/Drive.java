4bc4a6e517d1271bc8f332f29385fff737b2d625
Sara <Sara@DESKTOP-9UN18PT>
2021-04-06 14:38:31 -0600

    clase drive

---
+++ src/Logic/Drive.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LOGIC;
+
+/**
+ *
+ * @author Sara
+ */
+public class Drive {
+    //Attributes
+  private String name;
+   private String car;
+   private String qual;
+   private String pyment;
+   
+   public Drive() {
+    }
+    //Constructor
+    public Drive(String name1, String car, String qual, String pyment) {
+        this.name = name1;
+        this.car = car;
+        this.qual = qual;
+        this.pyment = pyment;
+    }
+    //Get and set
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public String getQual() {
+        return qual;
+    }
+
+    public void setQual(String qual) {
+        this.qual = qual;
+    }
+
+    public String getPyment() {
+        return pyment;
+    }
+
+    public void setPyment(String pyment) {
+        this.pyment = pyment;
+    }
+
+   
+  
+}
